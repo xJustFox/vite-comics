3ece1a2eeafab96d2fae4ab479a7e7f34181bcd6
xJustFox <dodydaversa@gmail.com>
2024-01-08 15:54:01 +0100

    [Upgrade] Created Header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
-<script setup>
-
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  export default{
+    components: {
+      AppHeader,
+    }
+  }
 </script>
 
 <template>
 
+  <AppHeader />
+
 </template>
 
 <style lang="scss">
-  @use './styles/generals.sass' as *;
-  @use './styles/partials/_variables.sass' as *;
+  @use './styles/generals.scss' as *;
+  @use './styles/partials/_variables.scss' as *;
+
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,93 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            links: [
+                {
+                    name: "CHARACTERS",
+                    href: "#",
+                },
+                {
+                    name: "COMICS",
+                    href: "#",
+                },
+                {
+                    name: "MOVIES",
+                    href: "#",
+                },
+                {
+                    name: "TV",
+                    href: "#",
+                },
+                {
+                    name: "GAMES",
+                    href: "#",
+                },
+                {
+                    name: "COLLECTIBLES",
+                    href: "#",
+                },
+                {
+                    name: "GAMES",
+                    href: "#",
+                },
+                {
+                    name: "VIDEOS",
+                    href: "#",
+                },
+                {
+                    name: "FANS",
+                    href: "#",
+                },
+                {
+                    name: "NEWS",
+                    href: "#",
+                },
+                {
+                    name: "SHOP",
+                    href: "#",
+                },
+            ]
+        }
+    },
+}
+</script>
+
+<template>
+    <header class="py-4">
+        <div class="container">
+            <nav class="row align-items-center">
+                <div class="col">
+                    <img src="../assets/imgs/dc-logo.png" alt="">
+                </div>
+                <div class="col">
+                    <ul class="d-flex list-unstyled m-0 ">
+                        <li class="px-2 " v-for="(link, index) in links" :key="index">
+                            <a :href="link.href">{{ link.name }}</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/_variables.scss' as *;
+
+    img {
+        height: 80px;
+    }
+
+    li a{
+        font-weight: 600;
+
+        &.active,
+        &:hover{
+            cursor: pointer;
+            color: #0282f9;
+        }
+    }
+
+</style>
