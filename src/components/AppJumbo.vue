6b5ff6760dcdb919e75f6b1a51109c4400b085e8
xJustFox <dodydaversa@gmail.com>
2024-01-09 17:16:47 +0100

    [Upgrade] Created AppJumbo

---
+++ src/components/AppJumbo.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'AppJumbo',
+}
+</script>
+
+<template lang="">
+    <div class="jumbo">
+        
+    </div>
+</template>
+
+<style lang="scss">
+    .jumbo{
+        height: 400px;
+        background-image: url('../assets/imgs/jumbotron.jpg');
+        background-size: cover;
+    }
+</style>
